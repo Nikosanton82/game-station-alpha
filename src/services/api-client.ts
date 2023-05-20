d8a21b9e9e66f35e76c55f37be152d13abd6e4f2
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-20 16:18:17 +0200

    Refactoring - Fetch Games with React Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,12 @@
 import axios from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[]
+}
+
 const apiKey = import.meta.env.VITE_API_KEY;
+
 
 if (!apiKey) {
   throw new Error('API_KEY is not defined');
