ebddaee999c644784d13437917cf56f231bccf5b
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-06-03 08:49:42 +0200

    Fetch Game Details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,35 +3,40 @@ import axios, { AxiosRequestConfig } from "axios";
 export interface FetchResponse<T> {
   count: number;
   next: string | null;
-  results: T[]
+  results: T[];
 }
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
-
 if (!apiKey) {
-  throw new Error('API_KEY is not defined');
+  throw new Error("API_KEY is not defined");
 }
 
 const axiosInstance = axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: apiKey
-    }
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: apiKey,
+  },
 });
 
 class APIClient<T> {
   endpoint: string;
 
   constructor(endpoint: string) {
-    this.endpoint = endpoint
+    this.endpoint = endpoint;
   }
 
   getAll(config: AxiosRequestConfig) {
     return axiosInstance
-    .get<FetchResponse<T>>(this.endpoint, config)
-    .then(res => res.data); 
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
   }
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
